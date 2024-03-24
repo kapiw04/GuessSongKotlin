ba840a579b5d17890048a592fd66ec1d44cb94b8
kapiw04 <wojtowicz.kacper@gmail.com>
2024-03-24 16:06:13 +0100

    Swiping songs is working

--- app/src/main/java/com/example/guesssong/SongsDisplayActivity.kt
+++ app/src/main/java/com/example/guesssong/SongsDisplayActivity.kt
@@ -1,31 +1,37 @@
 package com.example.guesssong
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager2.widget.ViewPager2
+import androidx.core.view.GestureDetectorCompat
 import java.util.Stack
+import kotlin.math.abs
 
-class SongsDisplayActivity : AppCompatActivity() {
+class SongsDisplayActivity : AppCompatActivity(),
+            GestureDetector.OnGestureListener
+{
 
-    private lateinit var viewPager: ViewPager2
+    private lateinit var text : TextView
     private var songsStack: Stack<Song> = Stack()
+
+    private lateinit var mDetector: GestureDetectorCompat
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.songs_display)
-        viewPager = findViewById(R.id.vpSongs)
+        setContentView(R.layout.song_item)
 
         populateSongs()
+        text = findViewById(R.id.tvSongTitle)
+        text.text = songsStack.peek().title
 
-        viewPager.adapter = SongsAdapter(songsStack.toList())
+        mDetector = GestureDetectorCompat(this, this)
+    }
 
-        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-            override fun onPageSelected(position: Int) {
-                super.onPageSelected(position)
-                moveToNextSong()
-            }
-        })
-
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        return mDetector.onTouchEvent(event) || super.onTouchEvent(event)
     }
 
     private fun populateSongs() {
@@ -34,12 +40,53 @@ class SongsDisplayActivity : AppCompatActivity() {
         }
     }
 
-    private fun moveToNextSong() {
-        if (songsStack.isNotEmpty()) {
+    @SuppressLint("SetTextI18n")
+    private fun nextSong() {
+        if (!songsStack.isEmpty()) {
             songsStack.pop()
-            (viewPager.adapter as? SongsAdapter)?.updateSongs(songsStack.toList())
-
-            viewPager.setCurrentItem(0, false)
+            if (!songsStack.isEmpty()) {
+                text.text = songsStack.peek().title
+            } else {
+                text.text = "No more songs"
+            }
         }
+    }
+
+    override fun onDown(e: MotionEvent): Boolean = true
+
+    override fun onShowPress(e: MotionEvent) {
+        // Do nothing
+    }
+
+    override fun onSingleTapUp(e: MotionEvent): Boolean = false
+
+    override fun onScroll(
+        e1: MotionEvent?,
+        e2: MotionEvent,
+        distanceX: Float,
+        distanceY: Float
+    ): Boolean = false
+
+    override fun onLongPress(e: MotionEvent) {
+        // Do nothing
+    }
+
+    override fun onFling(
+        e1: MotionEvent?,
+        e2: MotionEvent,
+        velocityX: Float,
+        velocityY: Float
+    ): Boolean {
+        e1?.let { start ->
+            e2.let { end ->
+                val deltaX = end.x - start.x
+                // Swipe left
+                if (abs(deltaX) > 100 && deltaX < 0) {
+                    nextSong()
+                    return true
+                }
+            }
+        }
+        return false
     }
 }
